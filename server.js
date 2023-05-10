3ec797f9af3b36bb74589c9ecd158e0757ec2210
unknown <bb3583400@gmail.com>
2023-05-11 00:48:01 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv =require('dotenv')
+const cors =require('cors')
+require('colors')
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+
+//config dotenv
+   dotenv.config()
+
+//connection
+connectDB()
+
+const app = express()
+
+  
+//middleware
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors('cors'))
+  
+// route
+app.use("/api/pizzas", require("./routes/pizzaRoute"))
+
+app.get('/',(req, res)=>{
+res.send('<h3> Node server Running </h3>')
+})
+const port = process.env.PORT || 8000
+app.listen(port, ()=>{
+    console.log('Server Running on port 8000')
+})
